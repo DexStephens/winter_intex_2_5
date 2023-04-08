b97a37d06f0f8711e1dc4a090ec1f82837006029
rachel-daniel <racheldaniel328@gmail.com>
2023-04-08 01:16:51 -0600

    added google authentication. need a place to store login info, so not 100% functional yet

--- Startup.cs
+++ Startup.cs
@@ -44,6 +44,17 @@ namespace winter_intex_2_5
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
             services.AddRazorPages();
+
+            // enable Google sign in
+            services.AddAuthentication()
+            .AddGoogle(options =>
+            {
+                IConfigurationSection googleAuthNSection =
+                    Configuration.GetSection("Authentication:Google");
+
+                options.ClientId = googleAuthNSection["ClientId"];
+                options.ClientSecret = googleAuthNSection["ClientSecret"];
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
