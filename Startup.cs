18ae53b07b7daa32b0402780e8f5715522a51a78
jakeevans <ejake370@gmail.com>
2023-04-10 15:03:49 -0600

    Attempt to fix http redirection

--- Startup.cs
+++ Startup.cs
@@ -88,6 +88,8 @@ namespace winter_intex_2_5
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseHttpsRedirection();
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -99,7 +101,7 @@ namespace winter_intex_2_5
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-            app.UseHttpsRedirection();
+            
             app.UseStaticFiles();
             app.UseCookiePolicy(); // GDPR cookie policy
 
