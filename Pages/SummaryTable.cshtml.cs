5455c2418e3e484305b6e46a14046322827cb384
Dexter Stephens <dexterfstephens@gmail.com>
2023-04-13 13:54:05 -0600

    Finish pagination with filtering.

--- Pages/SummaryTable.cshtml.cs
+++ Pages/SummaryTable.cshtml.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.IdentityModel.Tokens;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using winter_intex_2_5.Data.Repositories;
@@ -19,6 +20,7 @@ namespace winter_intex_2_5.Pages
         public bool ApplyFilter { get; set; }
         public int CurrentPage { get; set; }
         public int TotalPageCount { get; set; }
+        public bool RePost { get; set; } = false;
         public SummaryTableModel(IMummyRepository mummyRepository)
         {
             MummyRepository = mummyRepository;
@@ -33,21 +35,23 @@ namespace winter_intex_2_5.Pages
             TotalPageCount = AllSummaryTables.Count() / 50;
         }
 
-        public void OnPost(SummaryTableFilter summaryTableFilter, int currentPage = 1)
+        public void OnPost(SummaryTableFilter summaryTableFilter)
         {
-            CurrentPage = currentPage;
+            CurrentPage = Convert.ToInt32(Request.Form["currentPage"]);
             if(ModelState.IsValid)
             {
                 SummaryTableFilter = summaryTableFilter;
                 AllSummaryTables = new SummaryTableService(MummyRepository).FilterSummaryRowItemsByCriteria(summaryTableFilter);
-                CurrentSummaryTables = AllSummaryTables.Skip((currentPage - 1) * 50).Take(50);
+                CurrentSummaryTables = AllSummaryTables.Skip((CurrentPage - 1) * 50).Take(50);
                 SummaryTableDefaults = new SummaryTableDefaults(AllSummaryTables.Where(x => !string.IsNullOrWhiteSpace(x.Haircolor)).Select(x => x.Haircolor).Distinct().OrderBy(x => x), AllSummaryTables.Where(x => !string.IsNullOrWhiteSpace(x.Structure)).Select(x => x.Structure).Distinct().OrderBy(x => x), AllSummaryTables.Where(x => !string.IsNullOrWhiteSpace(x.Ageatdeath)).Select(x => x.Ageatdeath).OrderBy(x => x).Distinct(), AllSummaryTables.Where(x => !string.IsNullOrWhiteSpace(x.Headdirection) && x.Headdirection != "N LL").Select(x => x.Headdirection).Distinct().OrderBy(x => x), AllSummaryTables.Where(x => !string.IsNullOrWhiteSpace(x.Textilefunction)).Select(x => x.Textilefunction).Distinct().OrderBy(x => x), AllSummaryTables.Where(x => !string.IsNullOrWhiteSpace(x.Textilecolor)).Select(x => x.Textilecolor).Distinct().OrderBy(x => x), AllSummaryTables.Where(x => !string.IsNullOrWhiteSpace(x.Burialid)).Select(x => x.Burialid).Distinct());
                 TotalPageCount = AllSummaryTables.Count() / 50;
+                RePost = true;
             }
         }
 
         public IActionResult OnPostReset()
         {
+            RePost = false;
             return RedirectToPage();
         }
     }
